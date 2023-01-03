d1e37b9ce482d4ac6c0e37e5a4ab8174b8e6c334
Aseel3640 <aseelsaifaldin@yahoo.com>
2023-01-03 07:23:12 -0800

    p or n

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in str:
-        tmp = ord(i)
-        if tmp >= 97 and tmp <= 122:
-            pr = chr(tmp - 32)
-        else:
-            pr = i
-         print('{}'.format(pr), end="")
-    print()
+    for c in str:
+        if ord('a') <= ord(c):
+            c = chr(ord(c) - (ord('a') - ord('A')))
+        print("{:s}".format(c), end='')
+    print("")
