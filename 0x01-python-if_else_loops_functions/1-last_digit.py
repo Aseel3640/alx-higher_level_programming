764544010161b4976101116633882f1a4f405ed7
Aseel3640 <aseelsaifaldin@yahoo.com>
2023-01-03 07:38:25 -0800

    p or n

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if number >= 0
+    reminder = number  % -10
+else:  
     reminder = number  % 10
-elif number < 0:
-    remainder = ((number * -1) % 10) * -1
-
+print('Last digit of', number, 'is', remainder, end=' ')
 if remainder > 5:
     print('and is greater than 5')
 elif remainder == 0:
     print('and is 0')
-elif reminder < 6 and reminder != 0:
+else:
      print('and is less than 6 and not 0')
